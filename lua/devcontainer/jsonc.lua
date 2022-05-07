cb632a3d43612da68f08936bef1dfbe2b05f0b59
Ensar Sarajčić <es.ensar@gmail.com>
2022-05-07 21:13:16 +0200

    feat: use treesitter for jsonc parsing to cover more cases (#2)

--- lua/devcontainer/jsonc.lua
+++ lua/devcontainer/jsonc.lua
@@ -1,6 +1,24 @@
 local function clean_jsonc(jsonc_content)
-	local result, _ = string.gsub(jsonc_content, "//.-\n", "\n")
-	result, _ = string.gsub(result, "/\\*.-\\*/", "")
+	local parser = vim.treesitter.get_string_parser(jsonc_content, "jsonc")
+	local tree = parser:parse()
+	local root = tree[1]:root()
+	local query = vim.treesitter.parse_query("jsonc", "((comment)+ @c)")
+	local lines = vim.split(jsonc_content, "\n")
+
+	for _, node, _ in query:iter_captures(root) do
+		local row_start, col_start, row_end, col_end = node:range()
+		local line = row_start + 1
+		local start_part = string.sub(lines[line], 1, col_start)
+		local end_part = string.sub(lines[line], col_end + 1)
+		lines[line] = start_part .. end_part
+		for l = line + 1, row_end, 1 do
+			lines[l] = ""
+		end
+		if row_end + 1 ~= line then
+			lines[row_end + 1] = string.sub(lines[line], col_end + 1)
+		end
+	end
+	local result = vim.fn.join(lines, "\n")
 	return vim.fn.substitute(result, ",\\_s*}", "}", "g")
 end
 
--- tests/minimal.vim
+++ tests/minimal.vim
@@ -1,4 +1,14 @@
 set rtp+=.
 set rtp+=../plenary.nvim
+set rtp+=../nvim-treesitter
 
 runtime! plugin/plenary.vim
+runtime! plugin/nvim-treesitter.vim
+
+lua << EOF
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "jsonc" },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+}
+EOF
