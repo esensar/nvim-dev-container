esensar/nvim-dev-container

commit bc3f5c02fe04078a3388a9087ef6c996a2928947

fix: handle expansion of env placeholders with default values
This closes #87

lua/devcontainer/config_file/parse.lua
@@ -194,21 +194,35 @@ local function sub_variables(config_string)
   local local_workspace_folder_basename = parts[#parts]
   config_string = string.gsub(config_string, "${localWorkspaceFolder}", local_workspace_folder)
   config_string = string.gsub(config_string, "${localWorkspaceFolderBasename}", local_workspace_folder_basename)
-  config_string = string.gsub(config_string, "${localEnv:[a-zA-Z_]+[a-zA-Z0-9_]*}", function(part)
-    part = string.gsub(part, "${localEnv:", "")
-    part = string.sub(part, 1, #part - 1)
-    return vim.env[part] or ""
-  end)
+  config_string = string.gsub(
+    config_string,
+    "${localEnv:[a-zA-Z_]+[a-zA-Z0-9_]*(:[a-zA-Z_]+[a-zA-Z0-9_]*)?}",
+    function(part)
+      part = string.gsub(part, "${localEnv:", "")
+      part = string.sub(part, 1, #part - 1)
+      part = vim.split(part, ":")
+      part = part[1]
+      local default = part[2] or ""
+      return vim.env[part] or default
+    end
+  )
   -- TODO: containerWorkspaceFolder support
   return config_string
 end
 
 local function sub_container_env(config_string, env_map)
-  config_string = string.gsub(config_string, "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*}", function(part)
-    part = string.gsub(part, "${containerEnv:", "")
-    part = string.sub(part, 1, #part - 1)
-    return env_map[part] or ""
-  end)
+  config_string = string.gsub(
+    config_string,
+    "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*(:[a-zA-Z_]+[a-zA-Z0-9_]*)?}",
+    function(part)
+      part = string.gsub(part, "${containerEnv:", "")
+      part = string.sub(part, 1, #part - 1)
+      part = vim.split(part, ":")
+      part = part[1]
+      local default = part[2] or ""
+      return env_map[part] or default
+    end
+  )
   return config_string
 end
 
@@ -299,7 +313,7 @@ function M.remote_env_needs_fill(remote_env)
   })
 
   for _, v in pairs(remote_env) do
-    if string.match(v, "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*}") then
+    if string.match(v, "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*(:[a-zA-Z_]+[a-zA-Z0-9_]*)?}") then
       return true
     end
   end

lua/devcontainer/commands.lua
@@ -449,8 +449,8 @@ end
 local function run_docker_lifecycle_script(script, data, container_id)
   if type(script) == "string" then
     script = {
-      '/bin/sh',
-      '-c',
+      "/bin/sh",
+      "-c",
       script,
     }
   end

commit 55397a85f38591a4a9569c807d51e0f92745f5ec

fix: variable parsing pattern for defaults and no defaults
This was broken in bc3f5c02fe
This closes #87, this time properly

lua/devcontainer/config_file/parse.lua
@@ -68,7 +68,7 @@ end
 ---Parse specific devcontainer.json file into a Lua table
 ---Ensures that at least one of "image", "dockerFile" or "dockerComposeFile" keys is present
 ---@param config_file_path string
----@param callback function(err,data)|nil if nil run sync, otherwise run async and pass result to the callback
+---@param callback function|nil if nil run sync, otherwise run async and pass result to the callback(err, data)
 ---@return table|nil result or nil if running async
 ---@usage `require("devcontainer.config_file.parse").parse_devcontainer_config([[{ "image": "test" }]])`
 function M.parse_devcontainer_config(config_file_path, callback)
@@ -168,7 +168,7 @@ end
 ---Prefers .devcontainer.json over .devcontainer/devcontainer.json
 ---Looks in config.config_search_start first and then moves up all the way until root
 ---Fails if no devcontainer.json files were found, or if the first one found is invalid
----@param callback function(err,data)|nil if nil run sync, otherwise run async and pass result to the callback
+---@param callback function|nil if nil run sync, otherwise run async and pass result to the callback(err, data)
 ---@return table|nil result or nil if running async
 ---@usage `require("devcontainer.config_file.parse").parse_nearest_devcontainer_config()`
 function M.parse_nearest_devcontainer_config(callback)
@@ -196,13 +196,13 @@ local function sub_variables(config_string)
   config_string = string.gsub(config_string, "${localWorkspaceFolderBasename}", local_workspace_folder_basename)
   config_string = string.gsub(
     config_string,
-    "${localEnv:[a-zA-Z_]+[a-zA-Z0-9_]*(:[a-zA-Z_]+[a-zA-Z0-9_]*)?}",
+    "${localEnv:[a-zA-Z_]+[a-zA-Z0-9_]*:?[a-zA-Z_]*[a-zA-Z0-9_]*}",
     function(part)
       part = string.gsub(part, "${localEnv:", "")
       part = string.sub(part, 1, #part - 1)
       part = vim.split(part, ":")
-      part = part[1]
       local default = part[2] or ""
+      part = part[1]
       return vim.env[part] or default
     end
   )
@@ -213,13 +213,13 @@ end
 local function sub_container_env(config_string, env_map)
   config_string = string.gsub(
     config_string,
-    "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*(:[a-zA-Z_]+[a-zA-Z0-9_]*)?}",
+    "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*:?[a-zA-Z_]*[a-zA-Z0-9_]*}",
     function(part)
       part = string.gsub(part, "${containerEnv:", "")
       part = string.sub(part, 1, #part - 1)
       part = vim.split(part, ":")
-      part = part[1]
       local default = part[2] or ""
+      part = part[1]
       return env_map[part] or default
     end
   )
@@ -313,7 +313,7 @@ function M.remote_env_needs_fill(remote_env)
   })
 
   for _, v in pairs(remote_env) do
-    if string.match(v, "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*(:[a-zA-Z_]+[a-zA-Z0-9_]*)?}") then
+    if string.match(v, "${containerEnv:[a-zA-Z_]+[a-zA-Z0-9_]*:?[a-zA-Z_]*[a-zA-Z0-9_]*}") then
       return true
     end
   end
@@ -342,7 +342,7 @@ end
 ---Prefers .devcontainer.json over .devcontainer/devcontainer.json
 ---Looks in config.config_search_start first and then moves up all the way until root
 ---Fails if no devcontainer.json files were found, or if the first one found is invalid
----@param callback function(err,data)|nil if nil run sync, otherwise run async and pass result to the callback
+---@param callback function|nil if nil run sync, otherwise run async and pass result to the callback(err, data)
 ---@return string|nil result or nil if running async
 ---@usage `require("devcontainer.config_file.parse").find_nearest_devcontainer_config()`
 function M.find_nearest_devcontainer_config(callback)

lua/devcontainer/internal/executor.lua
@@ -45,7 +45,7 @@ end
 ---Passes all stdout and stderr data to opts.handler.stdout and opts.handler.stderr
 ---@param command string command to run
 ---@param opts RunCommandOpts contains stdio handlers as well as optionally options for vim.loop
----@param onexit function(code, signal)|nil
+---@param onexit function|nil function(code, signal)
 ---@see vim.loop opts.uv are passed to vim.loop.spawn
 ---@see https://github.com/luvit/luv/blob/master/docs.md
 ---@usage `require("devcontainer.internal.executor").run_command("docker", {}, function() end)`

tests/devcontainer/config_file/parse_spec.lua
@@ -7,6 +7,7 @@ local function mock_file_read(uv_mock, result, opts)
   uv_mock.fs_fstat.returns(opts.fstat or { size = string.len(result) })
   uv_mock.fs_read.returns(result)
   uv_mock.fs_close.returns(true)
+  uv_mock.hrtime.returns(0)
 end
 
 local function missing_file_func()
@@ -90,10 +91,7 @@ describe("devcontainer.config_file.parse:", function()
           local fstat = {
             size = string.len(file_content),
           }
-          uv_mock.fs_open.returns(mock_fd)
-          uv_mock.fs_fstat.returns(fstat)
-          uv_mock.fs_read.returns(file_content)
-          uv_mock.fs_close.returns(true)
+          mock_file_read(uv_mock, file_content, { fd = mock_fd, fstat = fstat })
 
           local success, _ = pcall(subject.parse_devcontainer_config, "test.json")
           assert.are.same(expected, success)
@@ -397,6 +395,7 @@ describe("devcontainer.config_file.parse:", function()
             extensions = {},
             containerEnv = {
               TEST_VAR = "${localEnv:TEST_VAR}",
+              MISSING_VAR = "${localEnv:MISSING_VAR:someDefault}",
               COMBINED_VARS = "${localEnv:COMBINED_VAR1}-${localEnv:COMBINED_VAR2}",
             },
             workspaceMount = "source=${localWorkspaceFolder},"
@@ -446,6 +445,10 @@ describe("devcontainer.config_file.parse:", function()
         assert.are.same("test_var_value", data.containerEnv.TEST_VAR)
         assert.are.same("var1_value-var2_value", data.containerEnv.COMBINED_VARS)
       end)
+
+      test_it("should use defaults for missing variables", function(data, _)
+        assert.are.same("someDefault", data.containerEnv.MISSING_VAR)
+      end)
     end)
   end)
 
@@ -489,6 +492,7 @@ describe("devcontainer.config_file.parse:", function()
         it(name, function()
           local remoteEnv = {
             TEST_VAR = "${containerEnv:TEST_VAR}",
+            MISSING_VAR = "${containerEnv:MISSING_VAR:someOtherDefault}",
             COMBINED_VARS = "${containerEnv:COMBINED_VAR1}-${containerEnv:COMBINED_VAR2}",
           }
           local env_map = {
@@ -506,6 +510,10 @@ describe("devcontainer.config_file.parse:", function()
       test_it("should update remoteEnv with replaced variables", function(data, _)
         assert.are.same("test_var_value", data.TEST_VAR)
         assert.are.same("var1_value-var2_value", data.COMBINED_VARS)
+      end)
+
+      test_it("should use defaults for missing variables", function(data, _)
+        assert.are.same("someOtherDefault", data.MISSING_VAR)
       end)
     end)
   end)